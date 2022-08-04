3330ee2848cd945b11ba9e50c9a2534b07978f06
Jessy Yeh <jiejun.yeh@gmail.com>
2022-08-04 18:00:15 +0100

    adjust styling

--- src/common/components/Nav/Nav.tsx
+++ src/common/components/Nav/Nav.tsx
@@ -1,24 +1,51 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import styles from "./Nav.module.css";
 
 export default function Nav() {
+  const router = useRouter();
+  const currentRoute = router.pathname;
   return (
     <nav className={styles.nav}>
       <ul className={styles.menuContainer}>
         <li className={styles.menu}>
           <Link href="/">
-            <a>Home</a>
+            <a
+              className={
+                currentRoute === "/"
+                  ? `${styles.active}`
+                  : `${styles["non-active"]}`
+              }
+            >
+              Home
+            </a>
           </Link>
         </li>
         <li className={styles.menu}>
           <Link href="/history">
-            <a>Mood History</a>
+            <a
+              className={
+                currentRoute === "/history"
+                  ? `${styles.active}`
+                  : `${styles["non-active"]}`
+              }
+            >
+              Mood History
+            </a>
           </Link>
         </li>
         <li className={styles.menu}>
           <Link href="/create">
-            <a>Create Mood</a>
+            <a
+              className={
+                currentRoute === "/create"
+                  ? `${styles.active}`
+                  : `${styles["non-active"]}`
+              }
+            >
+              Create Mood
+            </a>
           </Link>
         </li>
       </ul>
--- src/pages/create/Create.tsx
+++ src/pages/create/Create.tsx
@@ -1,80 +1,112 @@
 import { useState } from "react";
 
+import { API_URL } from "../../common/constants";
 import Nav from "../../common/components/Nav/Nav";
+
+import MoodRatingRadio from "./components/MoodRatingRadio/MoodRatingRadio";
+
+import styles from "./Create.module.css";
 
 export default function Create() {
   const [moodStatus, setMoodStatus] = useState("");
   const [date, setDate] = useState("");
   const [text, setText] = useState("");
 
-  function handleClick() {}
+  async function postMood() {
+    const postBody = {
+      status: moodStatus,
+      date: date,
+      text: text,
+    };
+
+    const res = await fetch(API_URL, {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(postBody),
+    });
+
+    const data = await res.json();
+    console.log(data);
+  }
 
   return (
     <>
       <Nav />
-      <h2>record your today&apos;s mood</h2>
-      <form>
-        <p>select your mood</p>
-        <input
-          type="radio"
-          onChange={(e) => setMoodStatus(e.target.value)}
-          name="mood"
-          value="happy"
-          id="happy"
-        />
-        <label htmlFor="happy">😀</label>
-        <input
-          type="radio"
-          onChange={(e) => setMoodStatus(e.target.value)}
-          name="mood"
-          value="sad"
-          id="sad"
-        />
-          <label htmlFor="sad">😢</label>
-        <input
-          type="radio"
-          onChange={(e) => setMoodStatus(e.target.value)}
-          name="mood"
-          value="angry"
-          id="angry"
-        />
-          <label htmlFor="angry">😠</label>
-        <input
-          type="radio"
-          onChange={(e) => setMoodStatus(e.target.value)}
-          name="mood"
-          value="anxious"
-          id="anxious"
-        />
-          <label htmlFor="anxious">😟</label>
-        <input
-          type="radio"
-          onChange={(e) => setMoodStatus(e.target.value)}
-          name="mood"
-          value="relaxed"
-          id="relaxed"
-        />
-          <label htmlFor="relaxed">😌</label>  <p>date</p>
-        <input
-          type="date"
-          id="start"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-          min="2022-01-01"
-          max="2090-12-31"
-        />
-        <p>description</p>
-        <label htmlFor="description"></label>
-        <input
-          name="description"
-          placeholder="write your feeling..."
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button type="submit" onClick={handleClick}>
-          submit
-        </button>
-      </form>
+      <div className={styles.formContainer}>
+        <h2 className={styles.title}>What&apos;s Your Mood Today?</h2>
+        <form className={styles.form}>
+          <p className={styles.subtitle}>I am feeling...</p>
+
+          <div className={styles.ratingContainer}>
+            <div className={styles.rating}>
+              <MoodRatingRadio
+                value="super-happy"
+                moodStatus={moodStatus}
+                setMoodStatus={setMoodStatus}
+              />
+              <MoodRatingRadio
+                value="happy"
+                moodStatus={moodStatus}
+                setMoodStatus={setMoodStatus}
+              />
+              <MoodRatingRadio
+                value="neutral"
+                moodStatus={moodStatus}
+                setMoodStatus={setMoodStatus}
+              />
+              <MoodRatingRadio
+                value="sad"
+                moodStatus={moodStatus}
+                setMoodStatus={setMoodStatus}
+              />
+              <MoodRatingRadio
+                value="super-sad"
+                moodStatus={moodStatus}
+                setMoodStatus={setMoodStatus}
+              />
+            </div>
+          </div>
+
+          <p className={styles.subtitle}>Date</p>
+
+          <label htmlFor="date"></label>
+          <input
+            className={styles.date}
+            type="date"
+            id="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            min="2022-01-01"
+            max="2090-12-31"
+          />
+          <p className={styles.subtitle}>
+            I am{" "}
+            <span className={styles.moodText}>
+              {{ moodStatus } ? moodStatus : `...`}
+            </span>{" "}
+            because...
+          </p>
+          <label htmlFor="description"></label>
+          <textarea
+            className={styles.text}
+            rows={3}
+            name="description"
+            placeholder="write your feeling..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button className={styles.submit} type="submit" onClick={postMood}>
+            submit
+          </button>
+        </form>
+      </div>
     </>
   );
 }
